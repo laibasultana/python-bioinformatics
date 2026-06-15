f592a67c9491d91600bf3e6bd5e9f762493d11a3
laibasultana <laibasultana75@gmail.com>
2026-06-15 16:47:40 +0500

    Add first Python program

---
+++ hello_bioinformatics.py
@@ -0,0 +1,11 @@
+# My first Python program
+# Written by Laiba Sultana
+
+name = "Laiba"
+field = "Bioinformatics"
+
+print("Hello World!")
+print("My name is", name)
+print("I am learning Python for", field)
+print("Day 1 of Python - Let's go!")
+
